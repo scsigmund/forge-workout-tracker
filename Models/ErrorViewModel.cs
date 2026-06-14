d6d81dd3323ac0771592470d39ebfdead1a5843b
Scott Sigmund <scsigmund@gmail.com>
2026-06-14 16:21:01 -0500

    Scaffold ASP.NET Core MVC project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace forge_workout_tracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
